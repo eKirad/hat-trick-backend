5b8d0f9b3669b1964ea2860b2439ed59f355441a
eKirad <evkirad@gmail.com>
2019-09-21 23:27:22 +0200

    Update interfaces

--- src/interfaces/IUser.ts
+++ src/interfaces/IUser.ts
@@ -1,12 +1,11 @@
 import mongoose from 'mongoose';
 
 export interface IUser extends mongoose.Document {
-    firstName: string,
-    familyName: string,
     username: string,
     password: string,
     role: string,
     email: string,
-    genedr: string,
-    dateOfBirth: string,
+    firstName?: string,
+    familyName?: string,
+    genedr?: string,
 }
--- src/models/User.ts
+++ src/models/User.ts
@@ -2,14 +2,13 @@ import mongoose, { Schema , Model } from 'mongoose';
 import { IUser } from '../interfaces/IUser';
 
 const userSchema: Schema = new Schema({
-    firstName: { type: String, required: true },
-    familyName: {type: String, required: true },
     username: { type: String, required: true, uniue: true },
     password: { type: String, required: true },
     role: { type: String, required: true },
     email: { type: String, required: true },
+    firstName: { type: String },
+    familyName: {type: String },
     genedr: { type: String },
-    dateOfBirth: { type: String }
 });
 
 export const User: Model<IUser> = mongoose.model<IUser>(`User`, userSchema);
