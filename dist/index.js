963687ad91a2714fe4abd1c0566c1fcb3371da2c
Evgeni <evkirad@gmail.com>
2020-03-21 20:40:38 +0100

    Add middlewares

--- dist/index.js
+++ dist/index.js
@@ -4,16 +4,19 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
-const body_parser_1 = __importDefault(require("body-parser"));
 const api_1 = require("./src/api/api");
 const config_1 = require("./src/config");
 const database_1 = require("./src/config/database");
+const middlewares_1 = require("./src/middlewares");
+// Set environment
 const ENV = config_1.environment;
 const app = express_1.default();
-app.use(body_parser_1.default.json());
-app.use(body_parser_1.default.urlencoded({
-    extended: false
-}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }));
+// Apply middlewares
+middlewares_1.middlewares(app);
 // Connect to the database
 database_1.connectToDB(config_1.config[ENV]);
 // Set all API Endpoints
--- index.ts
+++ index.ts
@@ -1,17 +1,22 @@
 import express from 'express';
-import bodyParser from 'body-parser';
+
 import { api } from './src/api/api';
 import { config, environment } from './src/config';
 import { connectToDB } from './src/config/database';
+import { middlewares } from './src/middlewares';
 
+// Set environment
 const ENV = environment;
 
 const app: express.Application = express();
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
-    extended: false
-}));
+// app.use(bodyParser.json());
+// app.use(bodyParser.urlencoded({
+//     extended: false
+// }));
+
+// Apply middlewares
+middlewares(app);
 
 // Connect to the database
 connectToDB(config[ENV]);
