222d7391bd29310affa86c9364433b119a00134a
Evgeni <evkirad@gmail.com>
2021-05-14 00:15:01 +0200

    :art: Adds new user.model

---
+++ src/models/user.model.ts
@@ -0,0 +1,14 @@
+import mongoose, { Schema, Document } from 'mongoose';
+import { User } from '../types/user.type';
+
+const UserSchema: Schema = new Schema({
+    email: { type: String, required: true, unique: true },
+    firstName: { type: String, required: true },
+    lastName: { type: String, required: true },
+    // createdAt: { type: Date },
+    // updatedAt: { type: Date },
+    // password: { type: String }
+});
+  
+const UserModel = mongoose.model<User & Document>('User', UserSchema);
+export default UserModel;
