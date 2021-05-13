0f8b5f57a02892a0ae8f6a8f386d813b198f7ee2
Evgeni <evkirad@gmail.com>
2021-05-14 00:16:44 +0200

    :construction: Updates auth controller & service

--- src/controllers/AuthController.ts
+++ src/controllers/AuthController.ts
@@ -6,37 +6,40 @@ import UserSignupDTO from '../dtos/auth/UserSignupDTO';
 import { plainToClass } from "class-transformer";
 import AuthService from '../services/AuthService';
 import UserLoginDTO from '../dtos/auth/UserLoginDTO';
+import { User } from '../types/user.type';
 
 export default class AuthController {
     public path = `/auth`;
     public router = express.Router();
     
+    private extractData = (requestBody: any): User => ({email: requestBody.email, firstName: requestBody.firstName, lastName: requestBody.lastName })
+
     constructor () { 
         this.intializeRoutes();
     }
 
     public intializeRoutes() {
         this.router.post(`/signup`, this.signup);
-        this.router.post(`/login`, this.login);
+        // this.router.post(`/login`, this.login);
     }
 
     protected signup = async (request: Request, response: Response, next: NextFunction) => {
         try {
-            const userSignupDTO: UserSignupDTO = plainToClass(UserSignupDTO, request.body);
-            await AuthService.signup(userSignupDTO)
+            const user = this.extractData(request.body);
+            await AuthService.signup(user)
             response.status(201).send({ msg: "Signup successfull" });
         } catch (e) {
             next(e);
         }
     }
 
-    protected login = async (request: Request, response: Response, next: NextFunction) => {
-        try {
-            const userLoginDTO: UserLoginDTO = plainToClass(UserLoginDTO, request.body);
-            const loginResponse = await AuthService.login(userLoginDTO);
-            response.status(200).send(loginResponse);
-        } catch (e) {
-            next(e);
-        }
-    }
+    // protected login = async (request: Request, response: Response, next: NextFunction) => {
+    //     try {
+    //         const userLoginDTO: UserLoginDTO = plainToClass(UserLoginDTO, request.body);
+    //         const loginResponse = await AuthService.login(userLoginDTO);
+    //         response.status(200).send(loginResponse);
+    //     } catch (e) {
+    //         next(e);
+    //     }
+    // }
 }
--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -3,59 +3,63 @@ import UserSignupDTO from "../dtos/auth/UserSignupDTO";
 import UserLoginDTO from "../dtos/auth/UserLoginDTO";
 
 // Enitity imports
-import { UserEntity, UserModel } from "../models/User";
+import UserModel from "../models/user.model";
 
 import IUserLoginResponse from "../api/types/IUserLoginResponse";
 import IUserAPI from "../api/types/IUserAPI";
 import * as jwt from 'jsonwebtoken';
 import { Config } from "../config/Config";
+import { User } from "../types/user.type";
 
 export default class AuthService {
-    
-    public static async signup(userRegisterDTO: UserSignupDTO): Promise<UserEntity> {
-        try {
-            const userEntity = new UserEntity(userRegisterDTO);
-            const emailCounter = await UserModel.countDocuments({ eMail: userEntity.eMail});
-            const usernameCounter = await UserModel.countDocuments({ username: userEntity.username});
-            
-            if (emailCounter > 0 || usernameCounter > 0) {
-                // throw new HttpException(409, "User with email already exists", "ENTITY_ALREADY_EXISTS");
-                console.log(`Entity already exits`);
-            }
-    
-            const user = await UserModel.create(userEntity);
 
+    
+    public static async signup(user: User): Promise<User> {
+        try {
+            
+            // const emailCounter = await UserModel.countDocuments({ eMail: userEntity.eMail});
+            const userModel = await UserModel.create(user);
+
+            // const userEntity = new UserEntity(userRegisterDTO);
+
+            // const usernameCounter = await UserModel.countDocuments({ username: userEntity.username});
+            
+            // if (emailCounter > 0 || usernameCounter > 0) {
+            //     // throw new HttpException(409, "User with email already exists", "ENTITY_ALREADY_EXISTS");
+            //     console.log(`Entity already exits`);
+            // }
+    
             return user;
         } catch(e) {
             console.error(e);
         }
     }
 
-    public static async login(userLoginDTO: UserLoginDTO): Promise<IUserLoginResponse> {
-        try {
+    // public static async login(userLoginDTO: UserLoginDTO): Promise<IUserLoginResponse> {
+    //     try {
 
-            const userEntity = await UserModel
-                .findOne({$or: [{ eMail: userLoginDTO.userIdentifier }, { username: userLoginDTO.userIdentifier }]});
+    //         const userEntity = await UserModel
+    //             .findOne({$or: [{ eMail: userLoginDTO.userIdentifier }, { username: userLoginDTO.userIdentifier }]});
         
-            if (!userEntity) {
-                console.log(`No user user with this email or username`);
-            }
+    //         if (!userEntity) {
+    //             console.log(`No user user with this email or username`);
+    //         }
     
-            if (!userEntity.isPasswodValid(userLoginDTO.password)) {
-                console.log(`Wrong password`);
-            }
+    //         if (!userEntity.isPasswodValid(userLoginDTO.password)) {
+    //             console.log(`Wrong password`);
+    //         }
     
-            const userAPI = userEntity.classToAPI();
-            const accessToken = AuthService.assignJWT(userAPI);
+    //         const userAPI = userEntity.classToAPI();
+    //         const accessToken = AuthService.assignJWT(userAPI);
 
-            return {
-                accessToken,
-                userAPI
-            }
-        } catch(e) {
-            console.error(e);
-        }
-    }
+    //         return {
+    //             accessToken,
+    //             userAPI
+    //         }
+    //     } catch(e) {
+    //         console.error(e);
+    //     }
+    // }
 
     public static assignJWT(userAPI: IUserAPI): string {
         return jwt.sign({
