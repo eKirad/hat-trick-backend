e02cb97f058d6e6aef4b98abeb30a5732e315123
Evgeni <evkirad@gmail.com>
2021-05-18 00:36:14 +0200

    :fire: Removes unused interfaces

--- src/api/types/IUserAPI.ts
+++
@@ -1,8 +0,0 @@
-export default interface IUserAPI {
-    id: string;
-    username: string,
-    email: string,
-    role?: string,
-    firstName?: string,
-    lastName?: string,
-}
--- src/api/types/IUserLoginResponse.ts
+++
@@ -1,6 +0,0 @@
-import IUserAPI from "./IUserAPI";
-
-export default interface IUserLoginResponse {
-    userAPI: IUserAPI;
-    accessToken: string;
-}
