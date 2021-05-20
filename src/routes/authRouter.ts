f51f1f2a24f6366fbd02584ca477df85c6254ed2
Evgeni <evkirad@gmail.com>
2021-05-20 19:24:45 +0200

    :art: Extends authRouter with login & signup functions

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -2,7 +2,11 @@ import * as express from 'express'
 import AuthController from '../controllers/authController';
 
 const router = express.Router();
+const path = `auth`
+
 router
-    .post("/login", new AuthController().signup)
+    .post(`/${path}/signup`, new AuthController().signup)
+    .post(`/${path}/login`, new AuthController().login)
+    .get(`/${path}/test`, new AuthController().test)
 
 export default router;
