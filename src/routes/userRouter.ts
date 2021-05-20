ba90b995ae92d1f7742f96e77ce9efc97d4101c6
Evgeni <evkirad@gmail.com>
2021-05-20 20:43:20 +0200

    :art: Adds userRouter

---
+++ src/routes/userRouter.ts
@@ -0,0 +1,11 @@
+import * as express from 'express'
+import AuthController from '../controllers/authController';
+import { UserController } from '../controllers/userController';
+
+const router = express.Router();
+const path = `user`
+
+router
+    .get(`/${path}`, new UserController().getAllResources)
+
+export default router;
