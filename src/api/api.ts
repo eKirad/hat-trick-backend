7462b703141c7c10e7e5427dd8d1d2012eb9113a
Evgeni <evkirad@gmail.com>
2021-05-20 19:16:48 +0200

    :art: Improves api.ts structure

--- src/api/api.ts
+++ src/api/api.ts
@@ -3,10 +3,8 @@ import express, { Application } from 'express';
 import router from '../routes';
 
 const contextPath = process.env.CONTEXT_PATH;
-const apiVersion = process.env.CONTEXT_PATH;
+const apiVersion = process.env.API_VERSION;
 
 export const api = (app: Application) => {
-    app.use(`${apiVersion}`, router)
-    // const authController = new AuthController();
-    // app.use(`${apiVersion}${authController.path}`, authController.router);
+    app.use(`/${contextPath}/${apiVersion}`, router)
 };
