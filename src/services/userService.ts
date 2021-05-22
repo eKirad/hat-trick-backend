f2a1dc75569306bb7d9f6e9cd0c76a75b632f1e6
Evgeni <evkirad@gmail.com>
2021-05-22 02:10:56 +0200

    :art: Updates userControlller

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -2,6 +2,6 @@ import UserModel from "../models/userModel";
 import { User } from "../types/userType";
 import { BaseService } from "./baseService";
 
-export default class UserService extends BaseService<User>{
-
+export default class UserService extends BaseService<User> {
+    public constructor() { super(UserModel) }
 }
