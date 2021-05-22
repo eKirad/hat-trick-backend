eb244fd51663fc26cc63e2b07d4f9ac499ff15a5
Evgeni <evkirad@gmail.com>
2021-05-22 23:20:38 +0200

    :art: Improves httpHandler structure

--- src/utils/httpHandlers.ts
+++ src/utils/httpHandlers.ts
@@ -2,5 +2,8 @@ import { EnforceDocument } from "mongoose";
 import { HttpResponse } from "../types/httpResponseType";
 import { Response as ExpressResponse } from 'express';
 
-export const httpResponse = <T>(response: ExpressResponse, statusCode: number, data?: EnforceDocument<T, {}>[]): HttpResponse<T> => response.status(statusCode)
-    .send({ statusCode, data })
+export const httpResponse = <T> (
+    response: ExpressResponse, 
+    statusCode: number, 
+    data?: EnforceDocument<T, {}> | EnforceDocument<T, {}>[]
+): HttpResponse<T> => response.status(statusCode).send({ statusCode, data })
