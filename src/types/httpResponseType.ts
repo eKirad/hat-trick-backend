6b295cb8ac82e85026ea6b34534354165836da22
Evgeni <evkirad@gmail.com>
2021-05-22 23:36:27 +0200

    :label: Updates httpResponseType and httpHandlers with StatusCodes type

--- src/types/httpResponseType.ts
+++ src/types/httpResponseType.ts
@@ -1,5 +1,7 @@
 import { EnforceDocument } from 'mongoose';
+import { StatusCodes } from 'http-status-codes';
+
 export type HttpResponse<T> = {
-    statusCode: number
+    statusCode: StatusCodes
     data?: EnforceDocument<T, {}> | EnforceDocument<T, {}>[]
 }
--- src/utils/httpHandlers.ts
+++ src/utils/httpHandlers.ts
@@ -1,9 +1,10 @@
 import { EnforceDocument } from "mongoose";
 import { HttpResponse } from "../types/httpResponseType";
 import { Response as ExpressResponse } from 'express';
+import { StatusCodes } from 'http-status-codes';
 
 export const httpResponse = <T> (
     response: ExpressResponse, 
-    statusCode: number, 
+    statusCode: StatusCodes, 
     data?: EnforceDocument<T, {}> | EnforceDocument<T, {}>[]
 ): HttpResponse<T> => response.status(statusCode).send({ statusCode, data })
