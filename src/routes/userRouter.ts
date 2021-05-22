230ffc353f89f993ce324906de946108bc960b83
Evgeni <evkirad@gmail.com>
2021-05-22 23:42:02 +0200

    :fire: Removes unused import

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,5 +1,4 @@
 import * as express from 'express'
-import AuthController from '../controllers/authController';
 import { UserController } from '../controllers/userController';
 
 const router = express.Router();
