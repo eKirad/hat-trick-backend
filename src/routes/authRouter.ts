29175976779241261a065f4d7cac7591cfafc069
Evgeni <evkirad@gmail.com>
2021-05-31 00:13:07 +0200

    :fire: Removes test function from authRouter

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -7,6 +7,5 @@ const path = `auth`
 router
     .post(`/${path}/signup`, new AuthController().signup)
     .post(`/${path}/login`, new AuthController().login)
-    .get(`/${path}/test`, new AuthController().test)
 
 export default router;
