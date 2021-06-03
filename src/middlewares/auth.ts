b034a602d6df8a2b13cdd4d8b1fb7db1cfd1ca27
Evgeni <evkirad@gmail.com>
2021-06-04 00:35:56 +0200

    :construction: Adds auth middleware

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -1,5 +1,19 @@
 import { Request, Response, NextFunction } from "express";
+import { StatusCodes } from "http-status-codes";
+import { createHttpErrorResponse } from "../utils";
 
+export const authMiddleware = (req: Request, res: Response, next: NextFunction) => { 
+    try {
+        const header = req.headers.authorization;
+        const [ , token ] = header && header.split(` `);
 
-
-export const authMiddleware = (req: Request, res: Response, next: NextFunction) => { }
+        if (!token) {
+            createHttpErrorResponse(res, StatusCodes.UNAUTHORIZED)
+        } else {
+            // TODO:
+        }
+        
+    } catch(e) {
+        next(e);
+    }
+};
