45e48cbe687f963db6f587371a5db551e34c402e
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-23 21:20:12 +0200

    :art: Updates authMiddleware with user in req

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,19 +1,16 @@
 import { Request, Response, NextFunction } from "express";
 import { StatusCodes } from "http-status-codes";
-import { createHttpErrorResponse } from "../utils";
+import { createHttpErrorResponse, verifyAccessToken } from "../utils";
 
 export const authMiddleware = (req: Request, res: Response, next: NextFunction) => { 
     try {
         const header = req.headers.authorization;
         const [ , token ] = header && header.split(` `);
-
-        if (!token) {
-            createHttpErrorResponse(res, StatusCodes.UNAUTHORIZED)
-        } else {
-            // TODO:
-        }
-        
-    } catch(e) {
-        next(e);
+        if (!token) createHttpErrorResponse(res, StatusCodes.UNAUTHORIZED);
+        req.user = verifyAccessToken(token);
+        next()
+    } catch(error) {
+        next(error);
+        // TODO: Add logger
     }
 };
