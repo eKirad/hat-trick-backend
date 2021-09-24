d456c77c3b94885a873fd9dfccf4e4165d067d23
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-25 00:58:09 +0200

    :bug: Fixes wrong i18next import

--- src/middlewares/common.ts
+++ src/middlewares/common.ts
@@ -1,7 +1,7 @@
 import { Application } from 'express';
 import * as bodyParser from 'body-parser'
 import * as i18nextMiddleware from 'i18next-http-middleware'
-import i18next from 'i18next';
+import i18next from '../shared/locales/i18next';
 
 export const commonMiddlewares = (app: Application) => {
     app.use(bodyParser.json());
