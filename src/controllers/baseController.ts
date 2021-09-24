aa36739e508e5ec36c0db737ffde9257c240156b
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-25 01:51:20 +0200

    :goal_net: Updates baseController with errors

--- src/controllers/baseController.ts
+++ src/controllers/baseController.ts
@@ -1,7 +1,7 @@
 import { Request, Response, NextFunction } from 'express';
 import { BaseService } from '../services/baseService';
 import { HttpResponse } from '../types';
-import { createHttpErrorResponse, createHttpResponse } from '../utils';
+import { createHttpErrorResponse, createHttpResponse, getMongooseCollectionDisplayName } from '../utils';
 import { StatusCodes } from 'http-status-codes';
 
 interface IGet<M> {
@@ -16,14 +16,14 @@ interface IModify<M> {
 }
 
 export class BaseController <M, S extends BaseService<M>> implements IGet<M>, IModify<M> {
-    constructor(private service: S) { }
-    
+    constructor(private service: S, private model: any) { }
+
     extractRequestBody = (requestBody: any) => requestBody;
 
     getAll = async ({ t }: Request, response: Response, next: NextFunction): Promise<HttpResponse<M>> => {
         try {
-            const model = await this.service.findAll()
-            return createHttpResponse<M>(response, StatusCodes.OK, model)
+            const models = await this.service.findAll()
+            return createHttpResponse<M>(response, StatusCodes.OK, models)
         } catch(e) {
             // TODO: Add logger
             throw createHttpErrorResponse(response, StatusCodes.INTERNAL_SERVER_ERROR, t("error:internal_server_error"))
@@ -34,6 +34,7 @@ export class BaseController <M, S extends BaseService<M>> implements IGet<M>, IM
         try {
             const id = params.id;
             const model = await this.service.findOne(id);
+            if (!model) throw createHttpErrorResponse(response, StatusCodes.NOT_FOUND, t("error:not_found", { collection: getMongooseCollectionDisplayName(this.model.collection.name) }))
             return createHttpResponse(response, StatusCodes.OK, model);
         } catch(e) {
             // TODO: Add logger
@@ -56,7 +57,7 @@ export class BaseController <M, S extends BaseService<M>> implements IGet<M>, IM
         try {
             const id = params.id;
             const dto = this.extractRequestBody(body);
-            const model = await this.service.update(id, dto);
+            const model = await this.service.update(id, dto); // TODO: Change with .findByIdAndUpdate()
             return createHttpResponse(response, StatusCodes.OK, model);
         } catch(e) {
             // TODO: Add logger
