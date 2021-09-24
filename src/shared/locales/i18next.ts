24fdf6096c8727aabd4c233613f534e11e4e04ed
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-25 01:15:51 +0200

    :art: Extracts i18next options

--- src/shared/locales/i18next.ts
+++ src/shared/locales/i18next.ts
@@ -1,23 +1,24 @@
-// TODO: Check for a TS way of handling it
-const i18next = require("i18next")
+import i18next, { InitOptions } from "i18next"
 import Backend from "i18next-fs-backend"
 import * as i18nextMiddleware from "i18next-http-middleware"
 import * as path from "path"
 
+const i18nextOptions: InitOptions = {
+    initImmediate: false,
+    backend: {
+        loadPath: path.join(__dirname, "/{{lng}}/{{ns}}.json")
+    },
+    detection: {
+        lookupHeader: "accept-language"
+    },
+    ns: ["middleware", "error"],
+    fallbackLng: "en",
+    preload: ["en"]
+}
+
 i18next
     .use(Backend)
     .use(i18nextMiddleware.LanguageDetector)
-    .init({
-        initImmediate: false,
-        backend: {
-            loadPath: path.join(__dirname, "/{{lng}}/{{ns}}.json")
-        },
-        detection: {
-            lookupHeader: "accept-language"
-        },
-        ns: ["middleware", "error"],
-        fallbackLng: "en",
-        preload: ["en"]
-    })
+    .init(i18nextOptions)
 
 export default i18next
