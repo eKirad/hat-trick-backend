51b6cf68e7a7d76d7adb07141c58e1e515d6f8de
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-26 00:14:23 +0200

    :fire: Removes loginValidationRules from the auth middleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -15,14 +15,3 @@ export const authMiddleware = (req: Request, res: Response, next: NextFunction) 
         // TODO: Add logger
     }
 };
-
-export const loginValidationRules = (): ValidationChain[] => [
-    check("email")
-        .exists()
-        .withMessage((_, { req: { t }}) => t("middleware:required", { field: "Email"}))
-        .isEmail()
-        .withMessage((_, { req: { t }}) => t("middleware:email_not_valid")),
-    check("password")
-        .exists()
-        .withMessage((_, { req: { t }}) => t("middleware:required", { fieled: "Password"}))
-];
