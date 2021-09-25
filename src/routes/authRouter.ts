9464813b62129622c8ded060e354fadeba4dade9
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-26 00:07:09 +0200

    :art: Adds loginValidation rules to the auth router

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,11 +1,13 @@
 import * as express from 'express'
 import AuthController from '../controllers/authController';
+import { loginValidationRules } from '../middlewares/authMiddleware';
+import { validateRules } from '../middlewares/validator';
 
 const router = express.Router();
 const path = `auth`
 
 router
     .post(`/${path}/signup`, AuthController.signup)
-    .post(`/${path}/login`, AuthController.login)
+    .post(`/${path}/login`, loginValidationRules(), validateRules, AuthController.login)
 
 export default router;
