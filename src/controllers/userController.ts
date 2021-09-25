b8025c40971e0adc728cf64c7e43573a7e85f874
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-25 03:26:29 +0200

    :fire: Removes unused UserModel from the controller

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -5,7 +5,7 @@ import { UserRegisterDTO, User } from "../types";
 import { BaseController } from "./baseController";
 
 class UserController extends BaseController<User, UserService> {
-    constructor() { super(new UserService(), UserModel) }
+    constructor() { super(new UserService()) }
 
     extractRequestBody = (requestBody: any): UserRegisterDTO => (
         { 
