4c1c1fbbca81e3f3c161667832ac74284e83d17f
Evgeni Kiradzhiyski <evkirad@gmail.com>
2021-09-26 22:22:53 +0200

    :art: UPdates authMiddleware

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -3,16 +3,16 @@ import { StatusCodes } from "http-status-codes";
 import logger from "../config/logger";
 import { createHttpErrorResponse, verifyAccessToken } from "../utils";
 
-export const authMiddleware = ({ headers, user, t}: Request, res: Response, next: NextFunction) => { 
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => { 
     try {
-        const header = headers.authorization;
+        const header = req.headers.authorization;
         const [ , token ] = header && header.split(` `);
-        if (!token) return createHttpErrorResponse(res, StatusCodes.UNAUTHORIZED, t("middleware:user_not_authorized"));
-        user = verifyAccessToken(token);
+        if (!token) return createHttpErrorResponse(res, StatusCodes.UNAUTHORIZED, req.t("middleware:user_not_authorized"));
+        req.user = await verifyAccessToken(token);
         next();
     } catch(e) {
         next(e);
         logger.error(`Authentication error occured: ${e}`);
-        throw createHttpErrorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, t("middleware:user_not_authorized"));
+        throw createHttpErrorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, req.t("middleware:user_not_authorized"));
     }
 };
