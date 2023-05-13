5a5f6db7dd3d1cfb34df094ff16ad2dfca32d705
eKirad <evkirad@gmail.com>
2023-05-13 09:55:59 +0300

    :bulb: Removes src code comments

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,21 @@
-import express from 'express';
+import express from "express"
 
-import { api } from './api/api';
-import { commonMiddlewares } from './middlewares';
-import Database from './config/database';
-import { Config } from './config/config';
-import logger from './config/logger';
+import { api } from "./api/api"
+import { commonMiddlewares } from "./middlewares"
+import Database from "./config/database"
+import { Config } from "./config/config"
+import logger from "./config/logger"
+;(async () => {
+    const config = new Config()
 
-(async () => {
-    // App config
-    const config = new Config();
-    
     try {
-        // Connect to the database
-        const database = new Database(config.dbURI, config.dbName, logger);
-        await database.connect();
-        const app = express();
-        // Apply middlewares
-        commonMiddlewares(app);
-        // Set up API
-        api(app);
-        app.listen(config.port, () => logger.info(`Listening on port ${config.port}...`));
-    } catch(e) {
-        logger.error(e);
+        const database = new Database(config.dbURI, config.dbName, logger)
+        await database.connect()
+        const app = express()
+        commonMiddlewares(app)
+        api(app)
+        app.listen(config.port, () => logger.info(`Listening on port ${config.port}...`))
+    } catch (e) {
+        logger.error(e)
     }
-})();
+})()
