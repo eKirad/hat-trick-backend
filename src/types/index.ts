bcb651caa4c6f6af2da4802d06fd8133498c04a1
eKirad <evkirad@gmail.com>
2023-05-13 10:43:29 +0300

    :truck: Moves types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,13 +1,6 @@
-import { User } from './user/userType';
-import { UserRegisterDTO, UserLoginDTO, UserResponse } from './user/userUtilityTypes';
-import { HttpResponse } from './common/httpResponseType';
-import { HttpErrorResponse } from './common/httpErrorResponseType';
+import { User } from "./user/userType"
+import { UserRegisterDTO, UserLoginDTO, UserResponse } from "./user/userUtilityTypes"
+import { HttpResponse } from "./httpTypes/httpResponseType"
+import { HttpErrorResponse } from "./httpTypes/httpErrorResponseType"
 
-export {
-    User,
-    UserResponse,
-    UserRegisterDTO,
-    UserLoginDTO,
-    HttpResponse,
-    HttpErrorResponse
-}
+export { User, UserResponse, UserRegisterDTO, UserLoginDTO, HttpResponse, HttpErrorResponse }
