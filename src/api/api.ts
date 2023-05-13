c5799ffff11b25f19bf0a783f985afa60d28d1d5
eKirad <evkirad@gmail.com>
2023-05-13 09:51:21 +0300

    :art: Updates API

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,7 @@
-import { Application } from 'express';
-import router from '../routes';
+import { Application } from "express"
+import router from "../routes"
 
-const contextPath = process.env.CONTEXT_PATH;
-const apiVersion = process.env.API_VERSION;
-export const api = (app: Application) => app.use(`/${contextPath}/${apiVersion}`, router);
+const contextPath = process.env.CONTEXT_PATH
+const apiVersion = process.env.API_VERSION
+
+export const api = (app: Application) => app.use(`/${contextPath}/${apiVersion}`, router)
