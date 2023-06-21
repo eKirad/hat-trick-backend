bfbc28837f955e0550f3573966b3f8a072b1434d
eKirad <evkirad@gmail.com>
2023-06-21 22:06:01 +0300

    :art: Adds scripts

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,39 +1,38 @@
-import UserModel from "../models/userModel";
-import * as jwt from 'jsonwebtoken';
-import { Config } from "../config/config";
-import { UserRegisterDTO, UserLoginDTO, User } from "../types";
-import { BaseService } from "./baseService";
-import * as bcrypt from 'bcryptjs';
-import { EnforceDocument } from "mongoose";
-import { omitMongooseObjectProp } from "../utils";
-import { UserResponse } from "../types";
+import UserModel from "../models/userModel"
+import * as jwt from "jsonwebtoken"
+import { Config } from "../config/config"
+import { UserRegisterDTO, UserLoginDTO, User } from "../types"
+import { BaseService } from "./baseService"
+import * as bcrypt from "bcryptjs"
+import { EnforceDocument } from "mongoose"
+import { omitMongooseObjectProp } from "../utils"
+import { UserResponse } from "../types"
 
 export default class AuthService extends BaseService<any> {
-
-    private static hashPassword = (plainPassword: string): string => bcrypt.hashSync(plainPassword);
+    private static hashPassword = (plainPassword: string): string => bcrypt.hashSync(plainPassword)
     private static isPasswordValid = (plainPassword: string, passwordHash: string): boolean => bcrypt.compareSync(plainPassword, passwordHash)
-    
+
     public static async signup(userDTO: UserRegisterDTO): Promise<EnforceDocument<UserResponse, {}>> {
         try {
             const createUser = {
                 ...userDTO,
                 password: this.hashPassword(userDTO.password),
                 dateCreateAt: new Date(),
-                lastUpdatedAt: new Date()
+                lastUpdatedAt: new Date(),
             }
 
-            const userModel = await UserModel.create(createUser);
-            const returnModel = omitMongooseObjectProp<EnforceDocument<UserResponse, {}>>(userModel, `password`);
-            return returnModel;
-        } catch(e) {
-            console.error(e);
+            const userModel = await UserModel.create(createUser)
+            const returnModel = omitMongooseObjectProp<EnforceDocument<UserResponse, {}>>(userModel, `password`)
+            return returnModel
+        } catch (e) {
+            console.error(e)
         }
     }
 
     public static async login(userDTO: UserLoginDTO): Promise<string> {
         try {
-            const userModel = await UserModel.findOne({ email: userDTO.email }).exec();
-            
+            const userModel = await UserModel.findOne({ email: userDTO.email }).exec()
+
             if (!userModel) {
                 // TODO: Throw an error
             }
@@ -42,20 +41,22 @@ export default class AuthService extends BaseService<any> {
                 // TODO: Throw an error
             }
 
-            return AuthService.assignJWT(userModel);
-        } catch(e) {
-            console.error(e);
+            return AuthService.assignJWT(userModel)
+        } catch (e) {
+            console.error(e)
         }
     }
 
-    public static assignJWT = (user:  EnforceDocument<User, {}>): string => jwt.sign(
-        {
-            email: user.email,
-            firstName: user.firstName,
-            lastName: user.lastName
-        },
-        new Config().authSecret, {
-            expiresIn: process.env.TOKEN_VALIDITY
-        }
-    );
+    public static assignJWT = (user: EnforceDocument<User, {}>): string =>
+        jwt.sign(
+            {
+                email: user.email,
+                firstName: user.firstName,
+                lastName: user.lastName,
+            },
+            new Config().authSecret,
+            {
+                expiresIn: process.env.TOKEN_VALIDITY,
+            }
+        )
 }
