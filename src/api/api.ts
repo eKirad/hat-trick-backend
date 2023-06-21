3e9cdbbc246d2769e783a6d08965e219c20637fc
eKirad <evkirad@gmail.com>
2023-06-21 21:49:01 +0300

    :recycle: Refactors imports

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,6 +1,5 @@
 import { Application } from "express"
 import router from "../routes"
-
 import { contextPath, apiVersion } from "../shared/consts"
 
 export const api = (app: Application) => app.use(`/${contextPath}/${apiVersion}`, router)
