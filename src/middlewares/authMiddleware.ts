30502d432edae9d46ea8670620b52a296987ddbd
eKirad <evkirad@gmail.com>
2023-09-25 23:07:10 +0200

    :recycle: Improves readability

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,18 +1,18 @@
-import { Request, Response, NextFunction } from "express";
-import { StatusCodes } from "http-status-codes";
-import logger from "../config/logger";
-import { createHttpErrorResponse, verifyAccessToken } from "../utils";
+import { Request, Response, NextFunction } from "express"
+import { StatusCodes } from "http-status-codes"
+import logger from "../config/logger"
+import { createHttpErrorResponse, verifyAccessToken } from "../utils"
 
-export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => { 
+export const authMiddleware = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const header = req.headers.authorization;
-        const [ , token ] = header && header.split(` `);
-        if (!token) return createHttpErrorResponse(res, StatusCodes.UNAUTHORIZED, req.t("middleware:user_not_authorized"));
-        req.user = await verifyAccessToken(token);
-        next();
-    } catch(e) {
-        next(e);
-        logger.error(`Authentication error occured: ${e}`);
-        throw createHttpErrorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, req.t("middleware:user_not_authorized"));
+        const header = req.headers.authorization
+        const [, token] = header && header.split(` `)
+        if (!token) return createHttpErrorResponse(res, StatusCodes.UNAUTHORIZED, req.t("middleware:user_not_authorized"))
+        req.user = await verifyAccessToken(token)
+        next()
+    } catch (error) {
+        next(error)
+        logger.error(`Authentication error ocurred: ${error}`)
+        throw createHttpErrorResponse(res, StatusCodes.INTERNAL_SERVER_ERROR, req.t("middleware:user_not_authorized"))
     }
-};
+}
