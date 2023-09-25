6a5cb01c31176b8cb8f3fd2649b2c62562cd2f16
eKirad <evkirad@gmail.com>
2023-09-25 20:55:37 +0200

    :truck: Adds index file for http types

---
+++ src/types/httpTypes/index.ts
@@ -0,0 +1,4 @@
+import { HttpErrorResponse } from "./httpErrorResponseType"
+import { HttpResponse } from "./httpResponseType"
+
+export { HttpResponse, HttpErrorResponse }
--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,5 @@
+import { HttpErrorResponse, HttpResponse } from "./httpTypes"
 import { User } from "./user/userType"
 import { UserRegisterDTO, UserLoginDTO, UserResponse } from "./user/userUtilityTypes"
-import { HttpResponse } from "./httpTypes/httpResponseType"
-import { HttpErrorResponse } from "./httpTypes/httpErrorResponseType"
 
 export { User, UserResponse, UserRegisterDTO, UserLoginDTO, HttpResponse, HttpErrorResponse }
