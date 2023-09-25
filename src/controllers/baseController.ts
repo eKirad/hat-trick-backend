87f2f6fdaf9c1e8c1bd23287b8a9cff325b2f2cc
eKirad <evkirad@gmail.com>
2023-09-25 08:51:20 +0200

    :recycle: Refactors based controller

--- src/controllers/baseController.ts
+++ src/controllers/baseController.ts
@@ -1,9 +1,9 @@
-import { Request, Response, NextFunction } from 'express';
-import { BaseService } from '../services/baseService';
-import { HttpResponse } from '../types';
-import { createHttpErrorResponse, createHttpResponse } from '../utils';
-import { StatusCodes } from 'http-status-codes';
-import logger from '../config/logger';
+import { Request, Response, NextFunction } from "express"
+import { BaseService } from "../services/baseService"
+import { HttpResponse } from "../types"
+import { createHttpErrorResponse, createHttpResponse } from "../utils"
+import { StatusCodes } from "http-status-codes"
+import logger from "../config/logger"
 
 interface IGet<M> {
     getOne(request: Request, response: Response, next: NextFunction): Promise<HttpResponse<M>>
@@ -16,43 +16,45 @@ interface IModify<M> {
     deleteOne(request: Request, response: Response, next: NextFunction): Promise<HttpResponse<M>>
 }
 
-export class BaseController <M, S extends BaseService<M>> implements IGet<M>, IModify<M> {
-    constructor(private service: S, private model: any) { }
+export class BaseController<M, S extends BaseService<M>> implements IGet<M>, IModify<M> {
+    constructor(private service: S, private model: any) {}
 
-    extractRequestBody = (requestBody: any) => requestBody;
+    extractRequestBody = (requestBody: any) => requestBody
 
     getAll = async ({ t }: Request, response: Response, next: NextFunction): Promise<HttpResponse<M>> => {
         try {
-            const models = await this.service.findAll();
-            return createHttpResponse<M>(response, StatusCodes.OK, models);
-        } catch(e) {
-            next(e);
-            logger.error(`Error occured at GET /${this.model.collection.name}: ${e}`)
-            throw createHttpErrorResponse(response, StatusCodes.INTERNAL_SERVER_ERROR, t("error:internal_server_error"));
+            const models = await this.service.findAll()
+
+            return createHttpResponse<M>(response, StatusCodes.OK, models)
+        } catch (error) {
+            next(error)
+            logger.error(`Error occured at GET /${this.model.collection.name}: ${error}`)
+            throw createHttpErrorResponse(response, StatusCodes.INTERNAL_SERVER_ERROR, t("error:internal_server_error"))
         }
     }
 
     getOne = async ({ params, t }: Request, response: Response, next: NextFunction) => {
         try {
-            const id = params.id;
-            const model = await this.service.findOneById(id, t);
-            return createHttpResponse(response, StatusCodes.OK, model);
-        } catch(e) {
-            next(e);
-            logger.error(`Error occured at GET /${this.model.collection.name}/${params.id}: ${e}`)
-            const errorMessage = e.message || t("error:internal_server_error");
-            const errorCode = e.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
-            throw createHttpErrorResponse(response, errorCode, errorMessage);
+            const id = params.id
+            const model = await this.service.findOneById(id, t)
+
+            return createHttpResponse(response, StatusCodes.OK, model)
+        } catch (error) {
+            next(error)
+            logger.error(`Error ocurred at GET /${this.model.collection.name}/${params.id}: ${error}`)
+            const errorMessage = error.message || t("error:internal_server_error")
+            const errorCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
+            throw createHttpErrorResponse(response, errorCode, errorMessage)
         }
     }
 
     createOne = async ({ body, t }: Request, response: Response, next: NextFunction) => {
         try {
-            const dto = this.extractRequestBody(body);
-            const model = await this.service.create(dto);
-            return createHttpResponse(response, StatusCodes.CREATED, model);
-        } catch(e) {
-            next(e);
+            const dto = this.extractRequestBody(body)
+            const model = await this.service.create(dto)
+            return createHttpResponse(response, StatusCodes.CREATED, model)
+        } catch (e) {
+            next(e)
             logger.error(`Error occured at POST /${this.model.collection.name}: ${e}`)
             throw createHttpErrorResponse(response, StatusCodes.INTERNAL_SERVER_ERROR, t("error:internal_server_error"))
         }
@@ -60,27 +62,27 @@ export class BaseController <M, S extends BaseService<M>> implements IGet<M>, IM
 
     updateOne = async ({ params, body, t }: Request, response: Response, next: NextFunction) => {
         try {
-            const id = params.id;
-            const dto = this.extractRequestBody(body);
-            const model = await this.service.updateOneById(id, dto, t);
-            return createHttpResponse(response, StatusCodes.OK, model);
-        } catch(e) {
-            next(e);
+            const id = params.id
+            const dto = this.extractRequestBody(body)
+            const model = await this.service.updateOneById(id, dto, t)
+            return createHttpResponse(response, StatusCodes.OK, model)
+        } catch (e) {
+            next(e)
             logger.error(`Error occured at PUT /${this.model.collection.name}/${params.id}: ${e}`)
-            const errorMessage = e.message || t("error:internal_server_error");
+            const errorMessage = e.message || t("error:internal_server_error")
             const errorCode = e.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
             throw createHttpErrorResponse(response, errorCode, errorMessage)
         }
     }
 
-    deleteOne = async ({ params, t}: Request, response: Response, next: NextFunction) => {
+    deleteOne = async ({ params, t }: Request, response: Response, next: NextFunction) => {
         try {
-            const id = params.id;
-            await this.service.delete(id);
-            return createHttpResponse(response, StatusCodes.OK);
-        } catch(e) {
+            const id = params.id
+            await this.service.delete(id)
+            return createHttpResponse(response, StatusCodes.OK)
+        } catch (e) {
             logger.error(`Error occured at DELETE /${this.model.collection.name}/${params.id}: ${e}`)
-            next(e);
+            next(e)
             throw createHttpErrorResponse(response, StatusCodes.INTERNAL_SERVER_ERROR, t("error:internal_server_error"))
         }
     }
