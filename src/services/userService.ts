098c0caa0a309dee5f58aec0dbc42c77f3541965
eKirad <evkirad@gmail.com>
2023-09-25 23:03:24 +0200

    :fire: Removes unused imports from user service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,11 +1,6 @@
 import UserModel from "../models/userModel"
-import { User, UserResponse } from "../types"
+import { User } from "../types"
 import { BaseService } from "./baseService"
-import { EnforceDocument } from "mongoose"
-import { TFunction } from "i18next"
-import HttpError from "../types/httpTypes/httpError"
-import { StatusCodes } from "http-status-codes"
-import { getMongooseCollectionDisplayName, omitMongooseObjectProp } from "../utils"
 import UserRepository from "../models/repositories/userRepository"
 
 class UserService extends BaseService<User, UserRepository> {
