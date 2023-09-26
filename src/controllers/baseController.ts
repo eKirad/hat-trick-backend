db26d8003892c96c26a1b7a402b3ee178a4d86b9
eKirad <evkirad@gmail.com>
2023-09-26 21:57:41 +0200

    :fire: Removes next function

--- src/controllers/baseController.ts
+++ src/controllers/baseController.ts
@@ -11,26 +11,24 @@ import { HttpRequest } from "../types/httpTypes/httpRequestType"
 export class BaseController<M, R extends BaseRepository<M>, S extends BaseService<M, R>> implements Get<M>, Modify<M> {
     constructor(private service: S, private model: any) {}
 
-    public getAll = async ({ t }: HttpRequest<M>, response: Response, next: NextFunction): Promise<HttpResponse<M>> => {
+    public getAll = async ({ t }: HttpRequest<M>, response: Response): Promise<HttpResponse<M>> => {
         try {
             const models = await this.service.findAll()
 
             return createHttpResponse<M>(response, StatusCodes.OK, models)
         } catch (error) {
-            next(error)
             logger.error(`Error ocurred at GET /${this.model.collection.name}: ${error}`)
             throw createHttpErrorResponse(response, StatusCodes.INTERNAL_SERVER_ERROR, t("error:internal_server_error"))
         }
     }
 
-    public getOne = async ({ params, t }: HttpRequest<M>, response: Response, next: NextFunction) => {
+    public getOne = async ({ params, t }: HttpRequest<M>, response: Response) => {
         try {
             const id = params.id
             const model = await this.service.findOneById(id, t)
 
             return createHttpResponse(response, StatusCodes.OK, model)
         } catch (error) {
-            next(error)
             logger.error(`Error ocurred at GET /${this.model.collection.name}/${params.id}: ${error}`)
             const errorMessage = error.message || t("error:internal_server_error")
             const errorCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
@@ -38,26 +36,24 @@ export class BaseController<M, R extends BaseRepository<M>, S extends BaseServic
         }
     }
 
-    public createOne = async ({ dto, t }: HttpRequest<M>, response: Response, next: NextFunction) => {
+    public createOne = async ({ dto, t }: HttpRequest<M>, response: Response) => {
         try {
             const model = await this.service.createOne(dto)
 
             return createHttpResponse(response, StatusCodes.CREATED, model)
         } catch (error) {
-            next(error)
             logger.error(`Error ocurred at POST /${this.model.collection.name}: ${error}`)
             throw createHttpErrorResponse(response, StatusCodes.INTERNAL_SERVER_ERROR, t("error:internal_server_error"))
         }
     }
 
-    public updateOne = async ({ params, dto, t }: HttpRequest<M>, response: Response, next: NextFunction) => {
+    public updateOne = async ({ params, dto, t }: HttpRequest<M>, response: Response) => {
         try {
             const id = params.id
             const model = await this.service.updateOneById(id, dto, t)
 
             return createHttpResponse(response, StatusCodes.OK, model)
         } catch (error) {
-            next(error)
             logger.error(`Error ocurred at PUT /${this.model.collection.name}/${params.id}: ${error}`)
             const errorMessage = error.message || t("error:internal_server_error")
             const errorCode = error.statusCode || StatusCodes.INTERNAL_SERVER_ERROR
@@ -65,7 +61,7 @@ export class BaseController<M, R extends BaseRepository<M>, S extends BaseServic
         }
     }
 
-    public deleteOne = async ({ params, t }: HttpRequest<M>, response: Response, next: NextFunction) => {
+    public deleteOne = async ({ params, t }: HttpRequest<M>, response: Response) => {
         try {
             const id = params.id
             await this.service.deleteOneById(id)
@@ -73,7 +69,6 @@ export class BaseController<M, R extends BaseRepository<M>, S extends BaseServic
             return createHttpResponse(response, StatusCodes.OK)
         } catch (error) {
             logger.error(`Error ocurred at DELETE /${this.model.collection.name}/${params.id}: ${error}`)
-            next(error)
             throw createHttpErrorResponse(response, StatusCodes.INTERNAL_SERVER_ERROR, t("error:internal_server_error"))
         }
     }
