eb8e6e59e7ecb99820b30b68966564cd509c4781
eKirad <evkirad@gmail.com>
2023-09-26 21:43:47 +0200

    :fire: Removes unused imports

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -2,7 +2,7 @@ import UserRepository from "../models/repositories/userRepository"
 import UserModel from "../models/userModel"
 import userService from "../services/userService"
 import UserService from "../services/userService"
-import { UserRegisterDTO, User } from "../types"
+import { User } from "../types"
 import { BaseController } from "./baseController"
 
 class UserController extends BaseController<User, UserRepository, typeof UserService> {
