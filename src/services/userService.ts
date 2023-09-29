e77bbd742c86eb8ee2bd29071d84e688e6f97820
eKirad <evkirad@gmail.com>
2023-09-29 19:47:59 +0200

    :fire: Cleans user service

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -3,8 +3,6 @@ import UserModel from "../models/user/user.schema"
 import { BaseService } from "./baseService"
 import UserRepository from "../models/repositories/userRepository"
 import { UserDTOs, UserResponse } from "../types/user/userUtilityTypes"
-import { Document, Require_id } from "mongoose"
-import * as crypto from "crypto"
 
 class UserService extends BaseService<UserDTOs, UserDocument, UserRepository> {
     constructor() {
@@ -16,14 +14,6 @@ class UserService extends BaseService<UserDTOs, UserDocument, UserRepository> {
         model.salt = undefined
 
         return model.toObject()
-    }
-
-    private createPasswordSalt = (): Buffer => Buffer.from(crypto.randomBytes(16).toString("hex"))
-
-    hashPassword = (plainTextPassword: string): string => {
-        const salt = this.createPasswordSalt()
-
-        return crypto.pbkdf2Sync(plainTextPassword, salt, 1000, 64, "sha1").toString("hex")
     }
 }
 
