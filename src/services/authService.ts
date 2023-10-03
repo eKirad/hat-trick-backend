85ced8d5d35387a341da3a07b2eccf692db4d37a
eKirad <evkirad@gmail.com>
2023-10-03 17:01:03 +0200

    :boom: Updates auth service

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,32 +1,38 @@
+import { Password } from "./../types/user/userPassword.type"
 import UserModel from "../models/user/user.schema"
 import * as jwt from "jsonwebtoken"
 import { Config } from "../config/config"
 import { UserRegisterDTO, UserLoginDTO } from "../types"
 import { BaseService } from "./baseService"
 
-import { omitMongooseObjectProp } from "../utils"
+import { createHttpErrorResponse, omitMongooseObjectProp } from "../utils"
 import { UserResponse } from "../types"
 import userService from "./userService"
 import { TFunction } from "i18next"
-import { Password } from "../types/user/userPassword.type"
+
 import * as crypto from "crypto"
+import { UserDocument } from "../models/user/user.types"
+import { omitMultipleMongooseObjectProps } from "../utils/objectHandlers"
+import * as bcrypt from "bcryptjs"
+import { StatusCodes } from "http-status-codes"
 
 export default class AuthService extends BaseService<any, any, any> {
-    private static createPasswordSalt = (): Buffer => Buffer.from(crypto.randomBytes(16).toString("hex"))
+    private static hashPassword = (plainPassword: string): string => bcrypt.hashSync(plainPassword)
+    private static isPasswordValid = (plainPassword: string, passwordHash: string): boolean => bcrypt.compareSync(plainPassword, passwordHash)
 
-    private static isPasswordValid = (user: any, password: string): boolean => {
-        const hash = crypto.pbkdf2Sync(password, user.salt, 1000, 64, "sha1").toString("hex")
-
-        return user.password === hash
-    }
-
-    private static convertUserDTOToModel = (userDTO: UserLoginDTO, { hash, salt }: Password) => ({
+    private static convertUserDTOToModel = (userDTO: UserLoginDTO, password: string) => ({
         ...userDTO,
-        password: hash,
-        salt,
+        password,
         dateCreateAt: new Date(),
         lastUpdatedAt: new Date(),
     })
+
+    private static convertUserModelToDTO = (userModel: UserDocument): UserResponse => {
+        const propsToOmit = [`password`, `salt`]
+        const userDTO = omitMultipleMongooseObjectProps<UserDocument, UserResponse>(userModel, propsToOmit)
+
+        return userDTO
+    }
 
     // TODO: Fix type
     private static assignJWT = (user: any): string =>
@@ -44,25 +50,25 @@ export default class AuthService extends BaseService<any, any, any> {
 
     public static async signup(userDTO: UserRegisterDTO): Promise<UserResponse> {
         try {
+            // const password = this.hashPassword(userDTO.password)
             const password = this.hashPassword(userDTO.password)
             const createUser = this.convertUserDTOToModel(userDTO, password)
-
-            const userModel = await UserModel.create(createUser)
-            const userResponse = omitMongooseObjectProp<UserResponse>(userModel, `password`)
+            const userModel = await userService.createOne(createUser)
+            const userResponse = this.convertUserModelToDTO(userModel)
 
             return userResponse
-        } catch (e) {
-            console.error(e)
+        } catch (error) {
+            console.error(error)
         }
     }
 
     public static async login(userDTO: UserLoginDTO, t: TFunction): Promise<string> {
         try {
-            const userModel = (await userService.findOne({ email: userDTO.email }, t)) as UserLoginDTO
+            const emailQuery = { email: userDTO.email }
+            const serviceQueryOptions = { shouldConvertToDTO: false }
+            const userModel = (await userService.findOne(emailQuery, t, serviceQueryOptions)) as any
 
-            if (!this.isPasswordValid(userDTO.password, userModel.password)) {
-                // TODO: Throw an error
-            }
+            if (!this.isPasswordValid(userDTO.password, userModel.password)) throw Error("Password is not valid")
 
             const accessToken = AuthService.assignJWT(userModel)
 
@@ -70,12 +76,5 @@ export default class AuthService extends BaseService<any, any, any> {
         } catch (error) {
             console.error(error)
         }
-    }
-
-    static hashPassword = (plainTextPassword: string): Password => {
-        const salt = this.createPasswordSalt()
-        const hash = crypto.pbkdf2Sync(plainTextPassword, salt, 1000, 64, "sha1").toString("hex")
-
-        return { hash, salt }
     }
 }
