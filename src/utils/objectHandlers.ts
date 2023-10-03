9acb71730529d51029f1f41e97a4fb226a712e38
eKirad <evkirad@gmail.com>
2023-10-03 16:58:00 +0200

    :art: Adds object util function

--- src/utils/objectHandlers.ts
+++ src/utils/objectHandlers.ts
@@ -1,5 +1,14 @@
 export const omitMongooseObjectProp = <T>(obj: any, prop: string): T => {
-    const { [prop]: omit, ...res } = obj._doc
+    const { [prop]: omit, ...res } = obj
 
     return res
 }
+
+export const omitMultipleMongooseObjectProps = <T, U>(mongooseModel: T, props: string[]): U =>
+    Object.keys(mongooseModel)
+        .filter((key) => !props.includes(key))
+        .reduce((result, key) => {
+            result[key] = mongooseModel[key]
+
+            return result
+        }, {} as U)
