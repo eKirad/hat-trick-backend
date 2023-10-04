26d21468b1dc4b25e796ed07428c6822d3a79db0
eKirad <evkirad@gmail.com>
2023-10-04 20:40:04 +0200

    :art: Adds requestWrapper to auth-related endpoints

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -3,10 +3,20 @@ import AuthController from "../controllers/authController"
 import { authValidationRules } from "../middlewares"
 import { validateRules } from "../middlewares"
 import { AUTH_PATH } from "./consts"
+import { requestWrapper } from "./requestWrapper"
 
 const router = express.Router()
 const path = AUTH_PATH
 
-router.post(`/${path}/signup`, authValidationRules(), validateRules, AuthController.signup).post(`/${path}/login`, authValidationRules(), validateRules, AuthController.login)
+const extractUserRequestBody = (requestBody: any): any => ({
+    email: requestBody.email,
+    password: requestBody.password,
+    firstName: requestBody.firstName,
+    lastName: requestBody.lastName,
+})
+
+router
+    .post(`/${path}/signup`, authValidationRules(), validateRules, requestWrapper(AuthController.signup, extractUserRequestBody))
+    .post(`/${path}/login`, authValidationRules(), validateRules, requestWrapper(AuthController.login, extractUserRequestBody))
 
 export default router
