18a509a892966e23b0b971cc700d0cb3e17bf521
eKirad <evkirad@gmail.com>
2023-10-04 07:50:21 +0200

    :label: Extends HttpError

--- src/types/httpTypes/httpError.ts
+++ src/types/httpTypes/httpError.ts
@@ -1,9 +1,11 @@
-import { StatusCodes } from 'http-status-codes';
+import { StatusCodes } from "http-status-codes"
 
 export default class HttpError extends Error {
-    public statusCode: StatusCodes;
-    constructor(statusCode: StatusCodes, message?: string) {
-        super(message);
-        this.statusCode = statusCode;
+    public statusCode: StatusCodes
+    public collectionName?: string
+    constructor(statusCode: StatusCodes, message?: string, collectionName?: string) {
+        super(message)
+        this.statusCode = statusCode
+        this.collectionName = collectionName
     }
 }
