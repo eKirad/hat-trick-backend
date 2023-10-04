961a2b16b17d4477b0b749d78d3d34139510e3a5
eKirad <evkirad@gmail.com>
2023-10-04 20:39:07 +0200

    :goal_net: Adds error handling to the request wrapper

--- src/routes/requestWrapper.ts
+++ src/routes/requestWrapper.ts
@@ -1,6 +1,8 @@
 import { Request, Response } from "express"
 import { HttpRequest } from "../types/httpTypes/httpRequestType"
 import { parseHttpRequest } from "../utils/request.utils"
+import { createHttpErrorResponse } from "../utils"
+import HttpError from "../types/httpTypes/httpError"
 
 export const requestWrapper =
     (controllerFunction: <T>(httpRequest?: HttpRequest<T>, res?: Response) => Promise<any>, extractBodyFunction?: <T>(requestBody: any) => T) =>
@@ -8,5 +10,8 @@ export const requestWrapper =
         try {
             const customHttpRequest = extractBodyFunction ? parseHttpRequest(req, extractBodyFunction) : parseHttpRequest(req)
             await controllerFunction(customHttpRequest, res)
-        } catch (error) {}
+        } catch (error) {
+            const { statusCode, errorMessage } = error
+            createHttpErrorResponse(res, statusCode, errorMessage)
+        }
     }
