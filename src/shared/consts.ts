4b9f1592efeeaeba0f50e36babe6a7077dd572f1
eKirad <evkirad@gmail.com>
2023-10-06 16:27:17 +0200

    :fire: Removes unnecessary consts

--- src/shared/consts.ts
+++ src/shared/consts.ts
@@ -1,14 +1,12 @@
 import { ConnectOptions } from "mongoose"
 import dotenv from "dotenv"
-import { EnvironmentEnum, RepositoryOptions } from "../types/common"
+import { RepositoryOptions } from "../types/common"
 import { ServiceQueryOptions } from "../types/common/service"
 
 dotenv.config()
 
-export const contextPath = process.env.CONTEXT_PATH
-export const apiVersion = process.env.API_VERSION
-export const isDev = process.env.NODE_ENV === EnvironmentEnum.DEV
-export const isProd = process.env.NODE_ENV === EnvironmentEnum.PROD
+// export const contextPath = process.env.CONTEXT_PATH
+// export const apiVersion = process.env.API_VERSION
 
 export const devDbConnectionOptions: ConnectOptions = { dbName: "test" }
 
