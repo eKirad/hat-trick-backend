260e111dffc2ad374b4a229198f2ae12a6424e8a
eKirad <evkirad@gmail.com>
2023-10-06 16:26:50 +0200

    :recycle: Refactors configs

--- src/config/config.ts
+++ src/config/config.ts
@@ -5,50 +5,62 @@ import { EnvironmentEnum } from "../types"
 
 dotenv.config()
 
+export interface DbConfigs {
+    dbUri: string
+    dbConnectOptions: ConnectOptions
+}
+
+export interface ApiConfigs {
+    contextPath: string
+    apiVersion: string
+}
+
 export class Config {
     private _env: EnvironmentEnum
     private _dbURI: string
     private _dbConnectOptions: ConnectOptions
     private _port: number
     private _authSecret: string
+    private _contextPath: string
+    private _apiVersion: string
 
     constructor() {
         this._env = process.env.NODE_ENV as EnvironmentEnum
-        this.setConfigsForEnv(this._env)
+        this.setConfigs()
     }
 
     get environment(): string {
         return this._env
     }
 
-    get dbURI(): string {
-        return this._dbURI
-    }
-
-    get dbConnectOptions(): ConnectOptions {
-        return this._dbConnectOptions
-    }
-
     get port(): number {
         return this._port
+    }
+
+    get dbConfigs(): DbConfigs {
+        return {
+            dbUri: this._dbURI,
+            dbConnectOptions: this._dbConnectOptions,
+        }
     }
 
     get authSecret(): string {
         return this._authSecret
     }
 
-    private setConfigsForEnv = (environment: EnvironmentEnum) => {
-        switch (environment) {
-            case EnvironmentEnum.DEV:
-                this._dbURI = process.env.DB_URI
-                this._port = Number(process.env.SERVER_PORT)
-                this._dbConnectOptions = devDbConnectionOptions
-                this._authSecret = process.env.JWT_SECRET
-                break
-            case EnvironmentEnum.PROD:
-                break
-            default:
-                break
+    get apiConfigs(): ApiConfigs {
+        return {
+            apiVersion: this._apiVersion,
+            contextPath: this._contextPath,
         }
+    }
+
+    private setConfigs = () => {
+        this._dbURI = process.env.DB_URI
+        this._port = Number(process.env.SERVER_PORT)
+        this._dbConnectOptions = devDbConnectionOptions
+        this._authSecret = process.env.JWT_SECRET
+        this._contextPath = process.env.CONTEXT_PATH
+        this._apiVersion = process.env.API_VERSION
     }
 }
