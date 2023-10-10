9af64367b856bf1b473598e8dfb0f7872e4aef52
eKirad <evkirad@gmail.com>
2023-10-10 18:03:07 +0200

    :recycle: Renames variable to improve readability

--- src/models/seed/users.ts
+++ src/models/seed/users.ts
@@ -6,7 +6,7 @@ import logger from "../../config/logger/winstonLogger"
 
 dotenv.config()
 
-const generateCommonUserData = ({ firstName, lastName, email, password }: UserRegisterDTO) => ({
+const generateUserData = ({ firstName, lastName, email, password }: UserRegisterDTO) => ({
     email,
     firstName,
     lastName,
@@ -18,9 +18,9 @@ const generateCommonUserData = ({ firstName, lastName, email, password }: UserRe
 export const generateUsers = async () => {
     logger.info(`Creating demo data ....`)
     try {
-        const user1 = generateCommonUserData({ email: "john@doe.com", firstName: "John", lastName: "Doe", password: process.env.DEFAULT_PASSWORD })
-        const user2 = generateCommonUserData({ email: "max@muster.com", firstName: "Max", lastName: "Musterman", password: process.env.DEFAULT_PASSWORD })
-        const user3 = generateCommonUserData({ email: "John@atanasoff.com", firstName: "John", lastName: "At", password: process.env.DEFAULT_PASSWORD })
+        const user1 = generateUserData({ email: "john@doe.com", firstName: "John", lastName: "Doe", password: process.env.DEFAULT_PASSWORD })
+        const user2 = generateUserData({ email: "max@muster.com", firstName: "Max", lastName: "Musterman", password: process.env.DEFAULT_PASSWORD })
+        const user3 = generateUserData({ email: "John@atanasoff.com", firstName: "John", lastName: "At", password: process.env.DEFAULT_PASSWORD })
 
         await userService.createMany([user1, user2, user3])
         logger.info(`Successfully created demo users.`)
