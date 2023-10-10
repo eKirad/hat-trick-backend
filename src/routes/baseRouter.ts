ce9a47d463a755aeb3b4245ba83472fc213f14b2
eKirad <evkirad@gmail.com>
2023-10-10 19:26:50 +0200

    :recycle: Renames base router argument
    Renames base router argument to improve readability

--- src/routes/baseRouter.ts
+++ src/routes/baseRouter.ts
@@ -3,13 +3,13 @@ import { authMiddleware } from "../middlewares"
 import { objectIdMiddleware } from "../middlewares"
 import { requestWrapper } from "./requestWrapper"
 
-export const generateBaseRoutes = (endpoint: string, controller: any, extractBody: any) => {
+export const generateBaseRoutes = (endpoint: string, controller: any, extractBodyFunction: any) => {
     const router = express.Router()
     router
         .get(`/${endpoint}`, authMiddleware, requestWrapper(controller.getAll))
         .get(`/${endpoint}/:id`, authMiddleware, objectIdMiddleware, requestWrapper(controller.getOne))
-        .post(`/${endpoint}`, authMiddleware, requestWrapper(controller.createOne, extractBody))
-        .put(`/${endpoint}/:id`, authMiddleware, objectIdMiddleware, requestWrapper(controller.updateOne, extractBody))
+        .post(`/${endpoint}`, authMiddleware, requestWrapper(controller.createOne, extractBodyFunction))
+        .put(`/${endpoint}/:id`, authMiddleware, objectIdMiddleware, requestWrapper(controller.updateOne, extractBodyFunction))
         .delete(`/${endpoint}/:id`, authMiddleware, objectIdMiddleware, requestWrapper(controller.deleteOne))
 
     return router
