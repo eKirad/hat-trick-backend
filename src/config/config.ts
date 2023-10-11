675cfaa35a3390f9688877d7f9e3192fba18d94a
eKirad <evkirad@gmail.com>
2023-10-11 08:14:10 +0200

    :art: Converts config to singleton

--- src/config/config.ts
+++ src/config/config.ts
@@ -2,6 +2,7 @@ import dotenv from "dotenv"
 import { ConnectOptions } from "mongoose"
 import { devDbConnectionOptions } from "../shared/consts"
 import { EnvironmentEnum } from "../types"
+import { parseBooleanEnv } from "../utils"
 
 dotenv.config()
 export interface DbConfigs {
@@ -18,6 +19,7 @@ export interface EnvConfigs {
     shouldRunDBSeed: boolean
 }
 export class Config {
+    private static instance: Config
     private _env: EnvironmentEnum
     private _dbURI: string
     private _dbConnectOptions: ConnectOptions
@@ -27,38 +29,9 @@ export class Config {
     private _apiVersion: string
     private _shouldRunDBSeed: boolean
 
-    constructor() {
+    private constructor() {
         this._env = process.env.NODE_ENV as EnvironmentEnum
         this.setConfigs()
-    }
-
-    get envConfigs(): EnvConfigs {
-        return {
-            env: this._env,
-            shouldRunDBSeed: this._shouldRunDBSeed,
-        }
-    }
-
-    get port(): number {
-        return this._port
-    }
-
-    get dbConfigs(): DbConfigs {
-        return {
-            dbUri: this._dbURI,
-            dbConnectOptions: this._dbConnectOptions,
-        }
-    }
-
-    get authSecret(): string {
-        return this._authSecret
-    }
-
-    get apiConfigs(): ApiConfigs {
-        return {
-            apiVersion: this._apiVersion,
-            contextPath: this._contextPath,
-        }
     }
 
     private setConfigs = () => {
@@ -68,6 +41,41 @@ export class Config {
         this._authSecret = process.env.JWT_SECRET
         this._contextPath = process.env.CONTEXT_PATH
         this._apiVersion = process.env.API_VERSION
-        this._shouldRunDBSeed = !!process.env.RUN_DB_SEED
+        this._shouldRunDBSeed = parseBooleanEnv(process.env.RUN_DB_SEED)
+    }
+
+    public get envConfigs(): EnvConfigs {
+        return {
+            env: this._env,
+            shouldRunDBSeed: this._shouldRunDBSeed,
+        }
+    }
+
+    public get port(): number {
+        return this._port
+    }
+
+    public get dbConfigs(): DbConfigs {
+        return {
+            dbUri: this._dbURI,
+            dbConnectOptions: this._dbConnectOptions,
+        }
+    }
+
+    public get authSecret(): string {
+        return this._authSecret
+    }
+
+    public get apiConfigs(): ApiConfigs {
+        return {
+            apiVersion: this._apiVersion,
+            contextPath: this._contextPath,
+        }
+    }
+
+    public static getInstance(): Config {
+        if (!Config.instance) Config.instance = new Config()
+
+        return Config.instance
     }
 }
