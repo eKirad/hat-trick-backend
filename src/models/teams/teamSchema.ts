088fdd3039bdf1295ee40bb0bc6bfce943944b18
eKirad <evkirad@gmail.com>
2023-11-04 16:43:00 +0100

    :card_file_box: Updates team schema and types

--- src/models/teams/teamSchema.ts
+++ src/models/teams/teamSchema.ts
@@ -4,13 +4,13 @@ import { TeamDocument } from "./teamTypes"
 const teamSchema = new Schema<TeamDocument>({
     name: { type: String, required: [true, "Name is required"] },
     shortName: { type: String, required: [true, "Short name is required"] },
-    nicknames: [{ type: String }],
     ground: { type: String, required: [true, "Ground is required"] },
     league: {
         type: Schema.Types.ObjectId,
         ref: "League",
         required: [true, "League is required"],
     },
+    nicknames: [{ type: String }],
     createdAt: { type: Date, default: () => new Date() },
     lastUpdatedAt: { type: Date, default: () => new Date() },
 })
--- src/models/teams/teamTypes.ts
+++ src/models/teams/teamTypes.ts
@@ -3,11 +3,11 @@ import { Document, Types } from "mongoose"
 export interface Team {
     name: string
     shortName: string
-    nicknames: Array<string>
     ground: string
     league: Types.ObjectId
-    createdAt: Date
-    lastUpdatedAt: Date
+    nicknames?: Array<string>
+    createdAt?: Date
+    lastUpdatedAt?: Date
 }
 
 export interface TeamDocument extends Team, Document {}
