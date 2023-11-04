0ccf67723a8251208a9542b5d091998bc57c6fd4
eKirad <evkirad@gmail.com>
2023-11-04 15:04:17 +0100

    :loud_sound: Updates user generation logs

--- src/services/dbSeedService.ts
+++ src/services/dbSeedService.ts
@@ -5,9 +5,9 @@ class DbSeedService {
     public runDBSeed = async () => {
         try {
             logger.info(`Running DB seed...`)
-            logger.info(`1. Generating demo users...`)
+
             await generateUsers()
-            logger.info(`Successfully generated demo users.`)
+
             logger.info(`Successfully ran DB seed.`)
         } catch (error) {
             console.error(`Error when running DB seed: ${error}`)
