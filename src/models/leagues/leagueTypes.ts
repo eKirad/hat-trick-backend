5ca79d5bc3d0952d0a033909b94c97f6579f5beb
eKirad <evkirad@gmail.com>
2023-11-05 11:52:50 +0100

    :bulb: Adds TODO

--- src/models/leagues/leagueTypes.ts
+++ src/models/leagues/leagueTypes.ts
@@ -3,6 +3,7 @@ import { Document } from "mongoose"
 export interface League {
     name: string
     shortCode: string
+    // TODO: Move to LeagueDocument
     createdAt: Date
     lastUpdatedAt: Date
 }
