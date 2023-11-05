63a22c6626015c6c4406e1f8a5786dc1b84e682d
eKirad <evkirad@gmail.com>
2023-11-05 12:21:57 +0100

    :label: Updates repo and service types

--- src/models/repositories/baseRepository.ts
+++ src/models/repositories/baseRepository.ts
@@ -1,9 +1,9 @@
-import { Model as MongooseModel, Document, Require_id } from "mongoose"
+import { Model as MongooseModel } from "mongoose"
 import { PopulatedFields, RepositoryOptions, RepositoryRead, RepositoryWrite } from "../../types"
 import { excludeFields } from "../../utils/database.utils"
 import { defaultRepositoryOptions } from "../../shared/consts"
 
-export class BaseRepository<T, D> implements RepositoryRead<D>, RepositoryWrite<T, D> {
+export class BaseRepository<DTO, DOCUMENT> implements RepositoryRead<DOCUMENT>, RepositoryWrite<DTO, DOCUMENT> {
     private populateFields: PopulatedFields[] = [
         {
             path: "",
@@ -13,9 +13,9 @@ export class BaseRepository<T, D> implements RepositoryRead<D>, RepositoryWrite<
 
     private excludedFields = []
 
-    public constructor(private Model: MongooseModel<D>) {}
+    public constructor(private Model: MongooseModel<DOCUMENT>) {}
 
-    public findAll = async (options: RepositoryOptions = defaultRepositoryOptions): Promise<Document<any, any, D>[]> => {
+    public findAll = async (options: RepositoryOptions = defaultRepositoryOptions): Promise<Array<DOCUMENT>> => {
         let models = await this.Model.find().exec()
 
         if (models.length === 0) return undefined
@@ -25,7 +25,7 @@ export class BaseRepository<T, D> implements RepositoryRead<D>, RepositoryWrite<
         return models
     }
 
-    public findOneById = async (id: string, options: RepositoryOptions = defaultRepositoryOptions): Promise<D | undefined> => {
+    public findOneById = async (id: string, options: RepositoryOptions = defaultRepositoryOptions): Promise<DOCUMENT | undefined> => {
         let model = await this.Model.findById(id).exec()
 
         if (!model) return undefined
@@ -39,7 +39,7 @@ export class BaseRepository<T, D> implements RepositoryRead<D>, RepositoryWrite<
         return model
     }
 
-    public findOne = async (data: any, options: RepositoryOptions = defaultRepositoryOptions): Promise<D | undefined> => {
+    public findOne = async (data: any, options: RepositoryOptions = defaultRepositoryOptions): Promise<DOCUMENT | undefined> => {
         let model = await this.Model.findOne(data).exec()
 
         if (!model) return undefined
@@ -53,7 +53,7 @@ export class BaseRepository<T, D> implements RepositoryRead<D>, RepositoryWrite<
         return model
     }
 
-    public createOne = async (dto: T, options: RepositoryOptions = defaultRepositoryOptions): Promise<Document<any, any, D>> => {
+    public createOne = async (dto: DTO, options: RepositoryOptions = defaultRepositoryOptions): Promise<DOCUMENT> => {
         let model = await this.Model.create(dto)
 
         if (options.populate) await this.Model.populate(model, this.populateFields)
@@ -65,7 +65,7 @@ export class BaseRepository<T, D> implements RepositoryRead<D>, RepositoryWrite<
         return model
     }
 
-    public updateOneById = async (id: string, dto: T): Promise<Document<any, any, D> | undefined> => {
+    public updateOneById = async (id: string, dto: DTO): Promise<DOCUMENT | undefined> => {
         const model = await this.Model.findByIdAndUpdate(id, dto, { new: true }).exec()
 
         if (!model) return undefined
@@ -75,7 +75,7 @@ export class BaseRepository<T, D> implements RepositoryRead<D>, RepositoryWrite<
 
     public deleteOneById = async (id: string): Promise<any> => await this.Model.findByIdAndDelete(id).exec()
 
-    public createMany = async (dtos: T[], _options: RepositoryOptions = defaultRepositoryOptions): Promise<any> => {
+    public createMany = async (dtos: Array<DTO>, _options: RepositoryOptions = defaultRepositoryOptions): Promise<any> => {
         const models = await this.Model.insertMany(dtos)
 
         return models
--- src/services/baseService.ts
+++ src/services/baseService.ts
@@ -6,18 +6,18 @@ import { TFunction } from "i18next"
 import { ServiceRead, ServiceWrite } from "../types"
 import HttpError from "../types/httpTypes/httpError.type"
 
-export class BaseService<T, D, R extends BaseRepository<T, D>> implements ServiceRead<T, D>, ServiceWrite<T> {
-    constructor(private repository: R, private model: any) {}
+export class BaseService<DTO, DOCUMENT, REPOSITORY extends BaseRepository<DTO, DOCUMENT>> implements ServiceRead<DTO, DOCUMENT>, ServiceWrite<DTO> {
+    constructor(private repository: REPOSITORY, private model: any) {}
 
-    modelToDTO = (model: D): T => ({} as T)
-    modelsToDTOs = (model: Document<any, any, D>[]): T[] => ({} as T[])
+    modelToDTO = (model: DOCUMENT): DTO => ({} as DTO)
+    modelsToDTOs = (model: Array<DOCUMENT>): Array<DTO> => ({} as Array<DTO>)
 
     private throwError = (statusCode: StatusCodes, t: TFunction) => {
         const collection = getMongooseCollectionDisplayName(this.model.collection.name)
         throw new HttpError(statusCode, t("error:not_found", { collection }), this.model.collection.name)
     }
 
-    public findAll = async (): Promise<T[]> => {
+    public findAll = async (): Promise<Array<DTO>> => {
         // TODO: Add error handling in case mdoels is `undefined`
         const models = await this.repository.findAll()
 
@@ -26,7 +26,7 @@ export class BaseService<T, D, R extends BaseRepository<T, D>> implements Servic
         return dtos
     }
 
-    public findOneById = async (id: string, t: TFunction): Promise<T | null> => {
+    public findOneById = async (id: string, t: TFunction): Promise<DTO | null> => {
         const model = await this.findOneDocumentById(id)
 
         if (!model) this.throwError(StatusCodes.NOT_FOUND, t)
@@ -36,13 +36,13 @@ export class BaseService<T, D, R extends BaseRepository<T, D>> implements Servic
         return dto
     }
 
-    public findOneDocumentById = async (id: string): Promise<D | null> => {
+    public findOneDocumentById = async (id: string): Promise<DOCUMENT | null> => {
         const model = await this.repository.findOneById(id)
 
         return model
     }
 
-    public findOne = async (data: any, t?: TFunction): Promise<T | null> => {
+    public findOne = async (data: any, t?: TFunction): Promise<DTO | null> => {
         const model = await this.findOneDocument(data)
 
         if (!model) this.throwError(StatusCodes.NOT_FOUND, t)
@@ -52,7 +52,7 @@ export class BaseService<T, D, R extends BaseRepository<T, D>> implements Servic
         return response
     }
 
-    public findOneDocument = async (data: any): Promise<D | null> => {
+    public findOneDocument = async (data: any): Promise<DOCUMENT | null> => {
         const model = await this.repository.findOne(data)
 
         return model
@@ -66,7 +66,7 @@ export class BaseService<T, D, R extends BaseRepository<T, D>> implements Servic
         return createdDTO
     }
 
-    public updateOneById = async (id: string, dto: T, t: TFunction): Promise<T> => {
+    public updateOneById = async (id: string, dto: DTO, t: TFunction): Promise<DTO> => {
         const updateModel = (await this.repository.updateOneById(id, dto)) as any
 
         if (!updateModel) this.throwError(StatusCodes.NOT_FOUND, t)
@@ -78,7 +78,7 @@ export class BaseService<T, D, R extends BaseRepository<T, D>> implements Servic
 
     public deleteOneById = async (id: string): Promise<any> => this.repository.deleteOneById(id)
 
-    public createMany = async (dtos: T[]): Promise<any> => {
+    public createMany = async (dtos: Array<DTO>): Promise<any> => {
         const models = await this.repository.createMany(dtos)
 
         const createdDTOs = this.modelsToDTOs(models)
