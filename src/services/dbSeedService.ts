0d6840ed9a722e748dfe3db816eb4d98bc171d46
eKirad <evkirad@gmail.com>
2024-10-14 00:01:01 +0200

    :seedling: Updates DB seed

--- src/services/dbSeedService.ts
+++ src/services/dbSeedService.ts
@@ -1,7 +1,7 @@
 import logger from "../config/logger/winstonLogger"
-import { generateEuro2024League, generateLeagues } from "../models/seed/leagues"
-import { generateEuro2024Teams, generateTeams } from "../models/seed/teams"
-import { generateUsers } from "../models/seed/users"
+// import { generateLeagues } from "../models/seed/leagues"
+// import { generateTeams } from "../models/seed/teams"
+// import { generateUsers } from "../models/seed/users"
 
 class DbSeedService {
     public runDBSeed = async () => {
@@ -10,7 +10,6 @@ class DbSeedService {
 
             // await generateUsers()
             // await generateEuro2024League()
-            await generateEuro2024Teams()
             // await generateTeams()
 
             logger.info(`Successfully ran DB seed.`)
