6fcb51a97229e61709bdd4f24964708423dc62b4
eKirad <evkirad@gmail.com>
2024-11-15 20:10:25 +0200

    :wrench: Extends common middlewares
    Extends common middlewares with cors

--- src/middlewares/commonMiddlewares.ts
+++ src/middlewares/commonMiddlewares.ts
@@ -1,4 +1,5 @@
 import { Application } from "express"
+import cors from "cors"
 import * as bodyParser from "body-parser"
 import * as i18nextMiddleware from "i18next-http-middleware"
 import i18next from "../shared/locales/i18next"
@@ -10,6 +11,7 @@ import { attachAuthSecretToRequest } from "./auth/attachAuthSecretMiddleware"
 export const commonMiddlewares = (app: Application, env: EnvironmentEnum, authSecret: string) => {
     app.use(bodyParser.json())
     app.use(bodyParser.urlencoded({ extended: false }))
+    app.use(cors())
     app.use(i18nextMiddleware.handle(i18next))
     app.use(attachAuthSecretToRequest(authSecret))
     const { format, options } = getMorganLoggerArgumentsForEnv(env)
